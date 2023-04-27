387612480d060d9ae8a7c7cd393d037ab425971d
kunal <contacttokunal@gmail.com>
2023-04-27 13:47:44 +0530

    conditional randing

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Fransisco", "Tokyo", "London"];
+  let items = ["New York", "San Fransisco", "Tokyo", "London"];
+  items = [];
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
+        {/* {items.length === 0 ? <p>No Item Found</p> : null} */}
+        {items.length === 0 && <p> No item Found</p>}
         {items.map((item) => (
           <li key={item} className="list-group-item">
             {item}
