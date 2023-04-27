0ac01c74ff406a6a8640e63b56d3181d44a943e0
kunal <contacttokunal@gmail.com>
2023-04-27 13:09:35 +0530

    create ListGroup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import ListGroup from "./components/ListGroup";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+}
 
 export default App;
