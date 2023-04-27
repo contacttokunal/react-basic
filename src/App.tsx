e45720990c78126b38150b0930384e49795cb3c5
kunal <contacttokunal@gmail.com>
2023-04-28 02:05:00 +0530

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = ["New York", "San Fransisco", "Tokyo", "London"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Hello World</span>
+      </Alert>
     </div>
   );
 }
