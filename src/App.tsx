fd0578e44054730c05b618fd78c6fdc5f751fd32
kunal <contacttokunal@gmail.com>
2023-05-04 01:59:58 +0530

    handling form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        suscipit doloribus odio, fugit nemo distinctio ex repellat ea aut
-        molestias necessitatibus voluptatum placeat dolore sit accusantium nobis
-        quae tempora, pariatur fuga explicabo ab? Doloremque, unde ducimus
-        repudiandae sint obcaecati neque hic, sunt laboriosam magnam esse
-        commodi at vel, temporibus iure consequuntur consequatur. Fuga optio
-        officiis natus laborum, molestiae temporibus blanditiis. Molestias,
-        illum voluptate cum, voluptates nulla iure nam eaque quasi quae,
-        laboriosam mollitia modi corporis! Porro cumque delectus perspiciatis
-        distinctio optio aut, ducimus voluptatum. Repudiandae pariatur harum,
-        minus est, architecto ab expedita consequatur aperiam ad quae rerum,
-        culpa quisquam tempore!
-      </ExpandableText>
+      <Form />
     </div>
   );
 };
