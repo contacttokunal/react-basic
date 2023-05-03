a906fd264b60eb53ad5f0704c4c258d79f410909
kunal <contacttokunal@gmail.com>
2023-05-03 14:12:22 +0530

    exercise 3 update array of object

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepproni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    // const items = { ...cart.items };
+    // const item = items.find((item) => item.id === 1);
+    // if (item) item.quantity = 2;
+    setCart({
+      ...cart,
+      items: {
+        ...cart.items.map((item) =>
+          item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+        ),
+      },
+    });
   };
 
   return (
