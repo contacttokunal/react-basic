d7a816abee74267917354d1004b877ee0d760fa7
kunal <contacttokunal@gmail.com>
2023-05-08 13:52:04 +0530

    Project-Expand tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./components/expense-tracker/ExpenseList";
+import ExpanseFilter from "./components/expense-tracker/ExpanseFilter";
+import ExpenseForm from "./components/expense-tracker/ExpenseForm";
+import categories from "./components/expense-tracker/categories";
 
 const App = () => {
+  const [selectedCategroy, setSeelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "abc", amount: 10, category: "Groceries" },
+    { id: 3, description: "acc", amount: 10, category: "Groceries" },
+  ]);
+
+  const visibleExpenses = selectedCategroy
+    ? expenses.filter((e) => e.category === selectedCategroy)
+    : expenses;
+
   return (
     <div>
-      <Form />
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpanseFilter
+          onSelectCategory={(category) => setSeelectedCategory(category)}
+        />
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) =>
+            setExpenses(expenses.filter((expense) => expense.id !== id))
+          }
+        />
+      </div>
     </div>
   );
 };
---
+++ src/components/expense-tracker/ExpenseList.tsx
@@ -0,0 +1,59 @@
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpandList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === 0) return null;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expanse) => (
+          <tr key={expanse.id}>
+            <td>{expanse.description}</td>
+            <td>{expanse.amount}</td>
+            <td>{expanse.category}</td>
+            <td>
+              <button
+                onClick={() => onDelete(expanse.id)}
+                className="btn btn-outline-danger"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            $
+            {expenses
+              .reduce((acc, expanse) => expanse.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ExpandList;
