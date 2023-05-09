d77b413b339a18a8261b4c0d7ec454b4b2c01261
kunal <contacttokunal@gmail.com>
2023-05-09 14:05:25 +0530

    Understanding the effect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
-import { useState } from "react";
-import ExpenseList from "./components/expense-tracker/ExpenseList";
-import ExpanseFilter from "./components/expense-tracker/ExpanseFilter";
-import ExpenseForm from "./components/expense-tracker/ExpenseForm";
-import categories from "./components/expense-tracker/categories";
+import React, { useEffect, useRef } from "react";
 
-const App = () => {
-  const [selectedCategroy, setSeelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "abc", amount: 10, category: "Groceries" },
-    { id: 3, description: "acc", amount: 10, category: "Groceries" },
-  ]);
+function App() {
+  const ref = useRef<HTMLInputElement>(null);
 
-  const visibleExpenses = selectedCategroy
-    ? expenses.filter((e) => e.category === selectedCategroy)
-    : expenses;
+  useEffect(() => {
+    //side Effect
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpanseFilter
-          onSelectCategory={(category) => setSeelectedCategory(category)}
-        />
-        <ExpenseList
-          expenses={visibleExpenses}
-          onDelete={(id) =>
-            setExpenses(expenses.filter((expense) => expense.id !== id))
-          }
-        />
-      </div>
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
-};
+}
 
 export default App;
